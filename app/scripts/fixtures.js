bafb2193c4462316ba61d602edd9b98f707a6a92
Zayne Berndt <berndt.zayne@gmail.com>
2016-11-11 15:11:34 +1000

    add controllers

---
+++ app/scripts/fixtures.js
@@ -0,0 +1,44 @@
+var albumPicasso = {
+    name: 'The Colors',
+    artist: 'Pablo Picasso',
+    label: 'Cubism',
+    year: '1881',
+    albumArtUrl: 'assets/images/album_covers/01.png',
+    songs: [
+      { name: 'Blue', length: 161.75, audioUrl: '/assets/music/blue' },
+      { name: 'Green', length: 103.96, audioUrl: '/assets/music/green' },
+      { name: 'Red', length: 268.45, audioUrl: '/assets/music/red' },
+      { name: 'Pink', length: 153.14, audioUrl: '/assets/music/pink' },
+      { name: 'Magenta', length: 374.22, audioUrl: '/assets/music/magenta' }
+    ]
+};
+
+var albumMarconi = {
+    name: 'The Telephone',
+    artist: 'Guglielmo Marconi',
+    label: 'EM',
+    year: '1909',
+    albumArtUrl: 'assets/images/album_covers/20.png',
+    songs: [
+      { name: 'Hello, Operator?', length: '1:01' },
+      { name: 'Ring, ring, ring', length: '5:01' },
+      { name: 'Fits in your pocket', length: '3:21' },
+      { name: 'Can you hear me now?', length: '3:14' },
+      { name: 'Wrong phone number', length: '2:15' }
+    ]
+};
+
+var albumTheDoors = {
+    name: 'The Doors',
+    artist: 'The Doors',
+    label: 'Elektra Records',
+    year: '1966',
+    albumArtUrl: 'assets/images/album_covers/22.png',
+    songs: [
+        { name: 'Break On Through (to the Other Side)', length: '2:26' },
+        { name: 'Soul Kitchen', length: '3:35' },
+        { name: 'Crystal Ship', length: '2:30'},
+        { name: 'Twentieth Century Fox', length: '2:34' },
+        { name: 'Alabama Song', length: '3:20'}
+    ]
+};
