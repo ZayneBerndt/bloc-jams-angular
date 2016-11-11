9c3ccb0e2c0ba2cc9f2115e2369d5df7f7171cf7
Zayne Berndt <berndt.zayne@gmail.com>
2016-11-11 15:35:45 +1000

    album controller

---
+++ app/scripts/controllers/AlbumCtrl.js
@@ -0,0 +1,10 @@
+function() {
+   function AlbumCtrl($scope) {
+     // this.albumData = albumPicasso;
+     $scope.albumData = albumPicasso;
+   };
+ 
+   angular
+     .module('blocJams')
+     .controller('AlbumCtrl', AlbumCtrl);
+ })();
