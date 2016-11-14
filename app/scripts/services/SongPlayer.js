20533a9aea99049725ebcc23e090beaa7f694256
Zayne Berndt <berndt.zayne@gmail.com>
2016-11-14 11:14:09 +1000

    songplayer service

--- app/scripts/services/SongPlayer.js
+++ app/scripts/services/SongPlayer.js
@@ -1,6 +1,47 @@
 (function() {
      function SongPlayer() {
           var SongPlayer = {};
+         
+         var currentSong = null;
+         /**
+ * @desc Buzz object audio file
+ * @type {Object}
+ */
+         var currentBuzzObject = null;
+         
+         /**
+ * @function setSong
+ * @desc Stops currently playing song and loads new audio file as currentBuzzObject
+ * @param {Object} song
+ */
+         
+         SongPlayer.play = function(song) {
+             
+             if (currentSong !== song) {
+            
+              else if (currentSong === song) {
+         if (currentBuzzObject.isPaused()) {
+             currentBuzzObject.play();
+         }
+     };
+                 
+        SongPlayer.pause = function(song) {
+        currentBuzzObject.pause();
+        setSong(song);
+        song.playing = false;
+ };
+                 
+         currentBuzzObject = new buzz.sound(song.audioUrl, {
+             formats: ['mp3'],
+             preload: true
+         });
+                 
+            currentSong = song;
+ 
+         currentBuzzObject.play(); 
+        song.playing = true;
+        }
+     };
           return SongPlayer;
      }
  
